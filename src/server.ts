5e4e9415bce7654fe83b9ee5d8d685eb19b404a0
mitchell <91237766+boring-jpg@users.noreply.github.com>
2025-04-01 20:08:15 -0400

    adjust server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+jimport express, { Request, Response } from 'express';
 import path from 'path';
 
 const __dirname = import.meta.dirname;
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the 'src' directory
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (_req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, 'index.html'));
